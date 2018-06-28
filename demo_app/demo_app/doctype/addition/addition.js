f1a09a9141d1c51ac4ce03a868556b2cfa4133b1
Harvindersingh0114 <harrysinghrocs@gmail.com>
2018-06-28 14:23:16 +0530

    Addition

--- demo_app/demo_app/doctype/addition/addition.js
+++ demo_app/demo_app/doctype/addition/addition.js
@@ -1,9 +1,8 @@
 // Copyright (c) 2018, frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Demo', {
+frappe.ui.form.on('Addition', {
 	refresh: function(frm) {
- 
+
 	}
- 
 });
